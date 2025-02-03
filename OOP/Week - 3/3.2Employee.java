f408f2046623a5757da31236bf73e7db8025124a
jrrom <77691121+jrrom@users.noreply.github.com>
2025-02-03 15:46:13 +0530

    Create 3.2Employee.java

---
+++ OOP/Week - 3/3.2Employee.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class Employee {
+	String Ename;
+	int Eid;
+	double Basic;
+	double DA;
+	double Gross_Sal;
+	double IT;
+	double Net_Sal;
+	
+	public void read() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter name: ");
+		this.Ename = sc.nextLine();
+		System.out.print("Enter id: ");
+		this.Eid = sc.nextInt();
+		System.out.print("Enter basic: ");
+		this.Basic = sc.nextDouble();
+		
+		sc.close();
+	}
+	
+	public void compute_net_sal() {
+		this.DA = Basic * 0.52;
+		this.Gross_Sal = Basic + DA;
+		this.IT = Gross_Sal * 0.3;
+		
+		this.Net_Sal = Gross_Sal - IT;
+	}
+	
+	public void display() {
+		System.out.println("Ename: " + Ename);
+		System.out.println("Eid: " + Eid);
+		System.out.println("Basic: " + Basic);
+		System.out.println("DA: " + DA);
+		System.out.println("IT: " + IT);
+		System.out.println("Gross Salary: " + Gross_Sal);
+		System.out.println("Net Salary: " + Net_Sal);
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		employee.read();
+		employee.compute_net_sal();
+		employee.display();
+	}
+}
